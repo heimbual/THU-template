2dffda008eb94afa313c4e0927f4adab0ac0f1ba
MelitaConeva <coneva@mail.hs-ulm.de>
2021-05-06 12:44:50 +0200

    Implemented csv reader method
    Ability to read csv files through a given file path - Example given in the comment above. Output via console.

--- src/main/java/profiles/ReaderWriter.java
+++ src/main/java/profiles/ReaderWriter.java
@@ -1,5 +1,10 @@
 package profiles;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReaderWriter {
 
 	
@@ -12,9 +17,27 @@ public class ReaderWriter {
 		//beim einlesen in Mapping speichern?
 	}
 	
-	public void readerCSV() {
+	//String filePath = new File("").getAbsolutePath() + File.separator + "/src/ihccOntology_Excerpt.csv";
+	public void readerCSV(String filePath) { 
+		try {
+            String line = "";
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                for(int i = 0; i<data.length; i++)
+                    System.out.print(data[i] + " ");
+                System.out.println("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (IndexOutOfBoundsException e) {
+             e.printStackTrace();
+        }
+    }
 		
-	}
+	
 
 	//Mapping speichern
 	public void writerJSON() {
