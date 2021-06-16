36f06c291aab4e74a85d2ad9c00811a7e080c37e
Albayrak Nurefsan <nur.albayrak@hotmail.de>
2021-06-16 16:54:05 +0200

    added the dictionary, splitted the link
    most of the dictionary is ready, but the connection to the profile class is missing. The link is splitted into valueCode and valueSystem.

--- src/main/java/profiles/Mapping.java
+++ src/main/java/profiles/Mapping.java
@@ -1,17 +1,14 @@
 package profiles;
-
 //managen von Daten
 
 public class Mapping {
-
+	
+	Profile profile= new Profile();
 	private String redCapId;
 	private String valueSystem;
 	private String valueCode;
+	private String completeID;
 	
-	public Mapping()
-	{
-		
-	}
 	
 	public Mapping(String redCapId, String valueSystem, String valueCode) {
 		this.redCapId = redCapId;
@@ -23,33 +20,39 @@ public class Mapping {
 		// redcap id speichern
 		// String teilen
 		// teile abspeichern
+		this.redCapId=redCapId;
+		this.completeID = completeID ;  //funktioniert das so überhaupt?	
+	}
+	
+	public void trennen(String completeID) {
+		String[] result = completeID.split("/",-5);
+		  
+		  System.out.println("Zero Limit split:");
+			for (String str2: result){
+			   System.out.println(str2);
+			}
+			//hier teilt es alle Wörter einzeln auf, was ich nicht will
+			//elegantere Lösung folgt !
+		
+		  valueSystem = result[0]+"/"+result[1]+"/"+result[2]+"/"+result[3]+"/"+result[4]+"/"; 
+		  valueCode = result[5];
+		  System.out.println(valueSystem);
+		  System.out.println(valueCode);
+		  //zu unpraktisch, aber für's erste funktioniert's und speichert die Werte ab
 	}
 	
 	public enum AdministrativeGender {
-        /**
-         * Male.
-         */
-        MALE, 
-        /**
-         * Female.
-         */
+        // Male.
+           MALE, 
+        //Female.
         FEMALE, 
-        /**
-         * Other.
-         */
+        //Other.
         OTHER, 
-        /**
-         * Unknown.
-         */
+        //Unknown.
         UNKNOWN, 
-        /**
-         * added to help the parsers
-         */
+        //added to help the parsers
         NULL;
 	}
-
-
-
 	public String getRedCapId() {
 		return redCapId;
 	}
@@ -73,4 +76,72 @@ public class Mapping {
 	public void setValueCode(String valueCode) {
 		this.valueCode = valueCode;
 	}
+	
+	
+	public void Wörterzuweisen() {
+		//unseren RedCap Server mit dem Ontology Server verbindende Wörterbuch 
+		//str1.contains("example")
+		//wenn unser Server das Wort beinhaltet und der ontology server das englische Wort hat, 
+		// soll mit der methode save die Wörter in der Hashmap zusammenkommen
+		
+		if (redCapId.contains("alter") & valueCode.contains("age")) {
+			  save();
+		}
+		
+		else if(redCapId.contains("größe") & valueCode.contains("height")) {
+			redCapId = valueCode;
+			
+		}
+		else if(redCapId.contains("gewicht") & valueCode.contains("weight")) {
+			save();
+		}
+		
+		else if(redCapId.contains("anatomical") & valueCode.contains("anatomical_reconstructibility")) {
+			save();
+		}
+		
+		else if(redCapId.contains("ascites") & valueCode.contains("ascites_observation")) {
+			save();
+		}
+		
+		else if(redCapId.contains("cholangitis") & valueCode.contains("cholangitis_observation")) {
+			save();
+		}
+		
+		else if(redCapId.contains("hepatitis_b") & valueCode.contains("chronic_hepatitis_b_observation")) {
+			save();
+		}
+		
+		else if(redCapId.contains("hepatitis_c") & valueCode.contains("chronic_hepatitis_c_observation")) {
+			save();
+		}
+		
+		else if(redCapId.contains("leberzirrhose") & valueCode.contains("cirrhosis_observation")) {
+			profile.addMapping(null);
+		}
+		
+		else if(redCapId.contains("COPD") & valueCode.contains("chronic_obstructive_pulmonary_disease_observe")) {
+			save();
+		}
+		
+		/*
+		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
+		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
+		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
+		 */
+		
+		else if(redCapId.contains(" ")) {
+			add(valueCode);
+		}
+	
+	//usw mit allen wörtern die in Oncology vorhanden sind
+}
+
+	private void add(String AddtoServer) {
+		redCapId= AddtoServer;
+	}
+
+	private void save() {		
+	}
+	
 }
