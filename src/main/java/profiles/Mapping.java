763291347a2aaa3d75b905a24e40a73295a3ac86
Albayrak Nurefsan <nur.albayrak@hotmail.de>
2021-06-17 12:46:49 +0200

    created method: initializeProfile
    predefined mappings to save them in hashmap (to be continued)

--- src/main/java/profiles/Mapping.java
+++ src/main/java/profiles/Mapping.java
@@ -3,31 +3,24 @@ package profiles;
 
 public class Mapping {
 	
-	Profile profile= new Profile();
 	private String redCapId;
 	private String valueSystem;
 	private String valueCode;
 	private String completeID;
 	
-	
-	public Mapping(String redCapId, String valueSystem, String valueCode) {
-		this.redCapId = redCapId;
-		this.valueSystem = valueSystem;
-		this.valueCode = valueCode;
-	}
-	
 	public Mapping(String redCapId, String completeID) {
 		// redcap id speichern
-		// String teilen
+		// String teilen -> split methode
 		// teile abspeichern
 		this.redCapId=redCapId;
 		this.completeID = completeID ;  //funktioniert das so �berhaupt?	
 	}
 	
-	public void trennen(String completeID) {
+	
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+	public void split(String completeID) {
 		String[] result = completeID.split("/",-5);
 		  
-		  System.out.println("Zero Limit split:");
 			for (String str2: result){
 			   System.out.println(str2);
 			}
@@ -53,6 +46,7 @@ public class Mapping {
         //added to help the parsers
         NULL;
 	}
+	
 	public String getRedCapId() {
 		return redCapId;
 	}
@@ -76,116 +70,4 @@ public class Mapping {
 	public void setValueCode(String valueCode) {
 		this.valueCode = valueCode;
 	}
-	
-	
-	public void W�rterzuweisen() {
-		//unseren RedCap Server mit dem Ontology Server verbindende W�rterbuch 
-		//str1.contains("example")
-		//wenn unser Server das Wort beinhaltet und der ontology server das englische Wort hat, 
-		// soll mit der methode save die W�rter in der Hashmap zusammenkommen
-		
-		if (redCapId.contains("alter") & completeID.contains("age")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("gr��e") & completeID.contains("height")) {
-			Mapping(redCapId, completeID);
-			
-		}
-		else if(redCapId.contains("gewicht") & completeID.contains("weight")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("anatomical") & completeID.contains("anatomical_reconstructibility")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("ascites") & completeID.contains("ascites_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("cholangitis") & completeID.contains("cholangitis_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("hepatitis_b") & completeID.contains("chronic_hepatitis_b_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("hepatitis_c") & completeID.contains("chronic_hepatitis_c_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("leberzirrhose") & completeID.contains("cirrhosis_observation")) {
-			profile.addMapping(null);
-		}
-		
-		else if(redCapId.contains("COPD") & completeID.contains("chronic_obstructive_pulmonary_disease_observe")) {
-			Mapping(redCapId, completeID);
-		}
-		//
-		else if(redCapId.contains("Fettleber") & completeID.contains("fatty_liver_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("COPD") & completeID.contains("chronic_obstructive_pulmonary_disease_observe")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("Herzinsuffizienz") & completeID.contains("heart_failure_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("Peritonealkarzinose") & completeID.contains("peritoneal_carcinomatosis_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("portale Hypertonie") & completeID.contains("portal_hypertension_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("Nierenversagen") & completeID.contains("renal_failure_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("Tumorbeobachtung") & completeID.contains("tumor_observation")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("chirurgische Rekonstruierbarkeit (Gallengang)") & completeID.contains("biliary_reconstruction_possible")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		else if(redCapId.contains("chirurgische Rekonstruierbarkeit (vaskulär)") & completeID.contains("vascular_reconstruction_possible")) {
-			Mapping(redCapId, completeID);
-		}
-		
-		//ASA nicht verstanden
-		
-		
-		/*
-		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
-		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
-		 * else if(redCapId.contains("age") & valueCode.contains("age")) { save(); }
-		 */
-		
-		else if(redCapId.contains(" ")) {
-			add(valueCode);
-		}
-	
-	//usw mit allen w�rtern die in Oncology vorhanden sind
-}
-
-	private void Mapping(String redCapId2, String completeID2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void add(String AddtoServer) {
-		redCapId= AddtoServer;
-	}
-
-	private void save() {		
-	}
-	
 }
--- src/main/java/profiles/Profile.java
+++ src/main/java/profiles/Profile.java
@@ -8,12 +8,12 @@ public class Profile {
 	
 	public Profile() {
 		this.mappings = new HashMap<String, Mapping>();
+		initializeProfile();
 	}
 	
 	public void addMapping(Mapping mapping) {
 		mappings.put(mapping.getRedCapId(), mapping);
 	}
-	
 	
 	public void deleteMapping(Mapping mapping) {
 		mappings.remove(mapping);
@@ -21,9 +21,17 @@ public class Profile {
 	
 	public Mapping getMapping(String redCapID) {
 		Mapping mapping = mappings.get(redCapID);
-		
 		return mapping;
-		
 	}
-
+	
+	private void initializeProfile() {
+		mappings.put("record_id ", new Mapping("record_id ", "meineGlobaleID"));
+		mappings.put("gender", new Mapping("gender", "http://sfb125.de/ontology/factorontology/gender"));
+		mappings.put("hepatitis_b ", new Mapping("hepatitis_b ", "http://sfb125.de/ontology/ihCCApplicationOntology/chronic_hepatitis_b_observation"));
+		mappings.put("bilirubin_concentration", new Mapping("bilirubin_concentration", "http://sfb125.de/ontology/ihCCApplicationOntology/bilirubin_concentration"));
+	
+		//Fortsetzung folgt 
+		mappings.put("MeineRedCapID", new Mapping("MeineRedcApID", "meineGlobaleID"));
+		mappings.put("MeineRedCapID", new Mapping("MeineRedcApID", "meineGlobaleID"));
+	}
 }
