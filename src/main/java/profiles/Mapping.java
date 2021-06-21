e67815bef13cc45de2f60a12b94707a33b70c759
Aleyna Yildiz <aleyna_yildiz@outlook.de>
2021-06-21 17:46:28 +0200

    more elegant solution for splitting the links

--- src/main/java/profiles/Mapping.java
+++ src/main/java/profiles/Mapping.java
@@ -10,7 +10,13 @@ public class Mapping {
 	
 	public Mapping(String redCapId, String completeID) {
 		// redcap id speichern
+// Updated upstream
 		// String teilen -> split methode
+
+		
+		// String teilen
+		
+// Stashed changes
 		// teile abspeichern
 		this.redCapId=redCapId;
 		this.completeID = completeID ;  //funktioniert das so �berhaupt?	
@@ -19,20 +25,20 @@ public class Mapping {
 	
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
 	public void split(String completeID) {
-		String[] result = completeID.split("/",-5);
+		String[] result = completeID.split("/",completeID.lastIndexOf("/"));
 		  
-			for (String str2: result){
-			   System.out.println(str2);
-			}
-			//hier teilt es alle W�rter einzeln auf, was ich nicht will
-			//elegantere L�sung folgt !
+			//for (String str2: result){
+			   //System.out.println(str2);
+			
+			
 		
-		  valueSystem = result[0]+"/"+result[1]+"/"+result[2]+"/"+result[3]+"/"+result[4]+"/"; 
-		  valueCode = result[5];
+		  valueSystem = result[0]; 
+		  valueCode = result[1];
 		  System.out.println(valueSystem);
 		  System.out.println(valueCode);
-		  //zu unpraktisch, aber f�r's erste funktioniert's und speichert die Werte ab
+		  
 	}
+
 	
 	public enum AdministrativeGender {
         // Male.
